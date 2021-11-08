1919474faab0e32b1a8ba96b0b20beaacda7f978
pragyamahajan <pmahajan8902@gmail.com>
2021-11-08 23:42:55 +0530

    change

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ let weather = {
         document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerHTML = "Wind speed: " + speed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
-        //document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+        
     },
         search: function() {
             this.fetchWeather(document.querySelector(".search-bar").value);
